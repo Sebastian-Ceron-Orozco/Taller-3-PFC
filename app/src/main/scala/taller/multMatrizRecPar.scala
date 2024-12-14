d60bee59eb505ece3e172ab5888589500aa2c79c
Juan Manuel Perez Cruz <144349006+JuanPerez27@users.noreply.github.com>
2024-12-13 22:14:05 -0500

    Correccion multMatrizRecPar.scala
    Se agrega una corrección funcional utilizando.par y eliminando las clausulas future

--- app/src/main/scala/taller/multMatrizRecPar.scala
+++ app/src/main/scala/taller/multMatrizRecPar.scala
@@ -1,66 +1,65 @@
-import scala.concurrent._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-
 package taller {
 
-class multMatrizRecPar{
+class MultiMatRecPar {
 
     type Matriz = Vector[Vector[Int]]
 
+    // Extrae una submatriz
     def subMatriz(m: Matriz, i: Int, j: Int, l: Int): Matriz = {
         m.slice(i, i + l).map(row => row.slice(j, j + l))
     }
 
+    // Suma dos matrices
     def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
-        val n = m1.length
-        require(m1.length == m2.length && m1(0).length == m2(0).length)
-        Vector.tabulate(n, n)((i, j) => m1(i)(j) + m2(i)(j))
+        m1.zip(m2).map { case (fila1, fila2) =>
+            fila1.zip(fila2).map { case (elem1, elem2) => elem1 + elem2 }
+        }
     }
 
+    // Multiplica matrices de manera recursiva con paralelización
     def multMatrizRecPar(matriz1: Matriz, matriz2: Matriz): Matriz = {
         val n = matriz1.length
-        require(matriz1.length == matriz2.length, "Matrices must be square and of the same size")
+        require(matriz1.length == matriz2.length, "Las matrices deben ser cuadradas y del mismo tamaño")
 
         if (n == 1) {
+            // Caso base: matrices 1x1
             Vector(Vector(matriz1(0)(0) * matriz2(0)(0)))
         } else {
+            // Dividir las matrices en submatrices
             val medio = n / 2
 
-            val matriz1A = subMatriz(matriz1, 0, 0, medio)
-            val matriz1B = subMatriz(matriz1, 0, medio, medio)
-            val matriz1C = subMatriz(matriz1, medio, 0, medio)
-            val matriz1D = subMatriz(matriz1, medio, medio, medio)
-
-            val matriz2A = subMatriz(matriz2, 0, 0, medio)
-            val matriz2B = subMatriz(matriz2, 0, medio, medio)
-            val matriz2C = subMatriz(matriz2, medio, 0, medio)
-            val matriz2D = subMatriz(matriz2, medio, medio, medio)
-
-            val futures = Seq(
-                Future(multMatrizRecPar(matriz1A, matriz2A)),
-                Future(multMatrizRecPar(matriz1B, matriz2C)),
-                Future(multMatrizRecPar(matriz1A, matriz2B)),
-                Future(multMatrizRecPar(matriz1B, matriz2D)),
-                Future(multMatrizRecPar(matriz1C, matriz2A)),
-                Future(multMatrizRecPar(matriz1D, matriz2C)),
-                Future(multMatrizRecPar(matriz1C, matriz2B)),
-                Future(multMatrizRecPar(matriz1D, matriz2D))
+            val (m1A, m1B, m1C, m1D) = (
+                subMatriz(matriz1, 0, 0, medio),
+                subMatriz(matriz1, 0, medio, medio),
+                subMatriz(matriz1, medio, 0, medio),
+                subMatriz(matriz1, medio, medio, medio)
             )
 
-            val results = Await.result(Future.sequence(futures), Duration.Inf)
+            val (m2A, m2B, m2C, m2D) = (
+                subMatriz(matriz2, 0, 0, medio),
+                subMatriz(matriz2, 0, medio, medio),
+                subMatriz(matriz2, medio, 0, medio),
+                subMatriz(matriz2, medio, medio, medio)
+            )
 
-            val top_left = sumMatriz(results(0), results(1))
-            val top_right = sumMatriz(results(2), results(3))
-            val bottom_left = sumMatriz(results(4), results(5))
-            val bottom_right = sumMatriz(results(6), results(7))
+            // Calcular las submatrices en paralelo usando `.par`
+            val resultados = Vector(
+                () => sumMatriz(multMatrizRecPar(m1A, m2A), multMatrizRecPar(m1B, m2C)),
+                () => sumMatriz(multMatrizRecPar(m1A, m2B), multMatrizRecPar(m1B, m2D)),
+                () => sumMatriz(multMatrizRecPar(m1C, m2A), multMatrizRecPar(m1D, m2C)),
+                () => sumMatriz(multMatrizRecPar(m1C, m2B), multMatrizRecPar(m1D, m2D))
+            ).par.map(task => task()).seq
 
+            val (c11, c12, c21, c22) = (resultados(0), resultados(1), resultados(2), resultados(3))
+
+            // Combinar las submatrices
             Vector.tabulate(n, n) { (i, j) =>
-                if (i < medio && j < medio) top_left(i)(j)
-                else if (i < medio && j >= medio) top_right(i)(j - medio)
-                else if (i >= medio && j < medio) bottom_left(i - medio)(j)
-                else bottom_right(i - medio)(j - medio)
+                if (i < medio && j < medio) c11(i)(j)
+                else if (i < medio && j >= medio) c12(i)(j - medio)
+                else if (i >= medio && j < medio) c21(i - medio)(j)
+                else c22(i - medio)(j - medio)
             }
         }
     }
-}}
+}
+}
